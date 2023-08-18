60cc20a445d5f370d0c4e6300b4ec357ae4e54f7
jainsrajan <jainsrajan77@gmail.com>
2023-08-18 18:27:41 +0530

    Updated

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -4,6 +4,9 @@ exports.home = (req , res)=>{
     res.send('Hello Srajan')
 }
 
+
+//Register User...
+
 exports.register = async (req , res)=>{
      
      try {
@@ -11,23 +14,26 @@ exports.register = async (req , res)=>{
         const{name , email , password} = req.body
 
         if(!name || !email || !password)
-        
+
         {
-            throw new Error('Name and Email are required');
+      res.status(500).send({msg:"Enter all the details of a user"})
         }
 
         //Saving data in database.
-        const user = await User.create({
+
+         await User.create({
                  name,
                  email,
                  password     
         })
      
-        res.status(201).json({
-         status:true,
-         message:'Registration is successfull',
-         user
-        })
+        // res.status(201).json({
+        //  status:true,
+        //  message:'Registration is successfull',
+        //  user
+         
+        // })
+        res.status(201).send({msg:"User registered successfully"})
         
      } catch (error) {
         console.log(error)
@@ -37,4 +43,34 @@ exports.register = async (req , res)=>{
         })
         
      }
+}
+
+
+//Login User....
+exports.login = async (req , res)=>{
+    const {email , password} = req.body
+
+    try {
+        const Userdata = await User.findOne({email})
+        
+        if(Userdata)
+
+        {
+            if(Userdata.password == password)
+            {
+                res.status(200).send({msg:"User Login Successfully, Enjoy!"})
+            }
+
+            else{
+                res.status(401).send({msg:"You have Entered Wrong Password"})
+            }
+        }
+        else{
+            res.status(404).send({msg:"No Account Find associated to this email"})
+        }
+        
+    } catch (error) {
+        res.status(501).send({msg:error.message})
+        
+    }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
-const{register, home} = require('../Controllers/controllers.js')
+const{register, home , login} = require('../Controllers/controllers.js')
 const router = express.Router()
 
 router.get('/' , home)
 router.post('/register' , register);
+router.post('/login' , login)
 
 
 module.exports = router;
